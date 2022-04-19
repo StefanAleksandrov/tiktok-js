e628b46cab8bd8f0a076e595bd6facb8f4b0b3a9
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2022-04-19 22:08:43 +0300

    Declaring variables in JavaScript

---
+++ 02.declaring-variables/index.js
@@ -0,0 +1,16 @@
+// Declaring variables in JS
+// var;
+// let;
+// const;
+
+function getNumber(condition) {
+    const number = 5;
+
+    if (condition) {
+        number = 6;
+    }
+
+    console.log(number);
+}
+
+getNumber(true);
