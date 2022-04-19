2c99e1f4a5cad0d96aa7c070439163e61ac7e5bf
Stefan Aleksandrov <arh.inj.alexandrov@abv.bg>
2022-04-19 22:08:30 +0300

    Mutating methods in JavaScript

---
+++ 01.array-mutate-methods/index.js
@@ -0,0 +1,11 @@
+// Array mutate methods in JS:
+// Array.prototype.pop();
+// Array.prototype.push();
+// Array.prototype.shift();
+// Array.prototype.unshift();
+const arr = [1, 2, 3, 4, 5];
+
+const length = arr.unshift(0);
+
+console.log(arr);
+console.log(length);
